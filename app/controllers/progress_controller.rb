e7551bf88abd157ca45d2094399a7e39cf6f4f46
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-03-14 11:11:02 -0700

    Update controller for progress entries route to handle a single progress entry or an array

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -9,17 +9,44 @@ class ProgressController < ApplicationController
 
   # POST /progress_entries
   def create
-    progress_entry = current_user.progress_entries.build(progress_entry_params) 
-    if progress_entry.save
-      render json: progress_entry, status: :created
+    if params[:progress_entries].is_a?(Array)
+      created_entries = []
+      errors = []
+
+      multiple_progress_entries_params.each do |entry_params|
+        progress_entry = current_user.progress_entries.build(entry_params)
+
+        if progress_entry.save
+          created_entries << progress_entry
+        else
+          errors << {entry: entry_params, error: progress_entry.errors.full_messages }
+        end
+      end
+
+      if errors.empty?
+        render json: created_entries, status: :created
+      else
+        render json: { error: errors}, status: :unprocessable_entity
+      end
+
     else
-      render json: { errors: progress_entry.errors.full_messages }, status: :unprocessable_entity
-    end
+      progress_entry = current_user.progress_entries.build(progress_entry_params) 
+      if progress_entry.save
+        render json: progress_entry, status: :created
+      else
+        render json: { errors: progress_entry.errors.full_messages }, status: :unprocessable_entity
+      end
   end
 
   private
 
   def progress_entry_params
     params.require(:progress_entry).permit(:image_id, :day, :region_id, :color)
+  end
+
+  def multiple_progress_entiers_params
+    params.require(:progress_entries).map do |entry|
+      entry.permit(:image_id, :day, :region_id, :color)
+    end
   end
 end
